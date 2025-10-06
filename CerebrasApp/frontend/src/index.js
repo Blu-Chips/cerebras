0940e63692675dc27aee1f93bf8741d5368168a1
Blu-Chips <141764134+Blu-Chips@users.noreply.github.com>
2025-10-06 12:14:33 +0300

    commit

--- CerebrasApp/frontend/src/index.js
+++ CerebrasApp/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import  ColorModeScript  from '@chakra-ui/react/color-mode';  
+import  ColorModeScript  from '@chakra-ui/react';  
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
